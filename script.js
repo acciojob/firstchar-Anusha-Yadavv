ebf34e7c06740310a3a27f6ec1b738b6e2a5c296
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-16 09:10:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function firstChar(text) {
   // your code here
 	let first_char=text[0];
-	return first_char;
+	// return first_char
+	if(text.length==1 && first_char==" "){
+		return " ";
+	}
+	else if(first_char!==" " && text.length>2){
+		return first_char;
+	}
 	
 }
 
